f1093d8c75dfce88454ad8f5f48d31ab4be044c2
bells bonzanini <izabelabonzanini@gmail.com>
2023-10-18 14:39:30 -0300

    criação do CRUD e da categoria no projeto da farmácia, na parte de react.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -8,8 +8,8 @@ function Home() {
       <div className="bg-emerald-300 flex justify-center">
         <div className='container grid grid-cols-2 text-white'>
           <div className="flex flex-col gap-4 items-center justify-center py-4">
-            <h2 className='text-5xl text-black font-bold'>Seja bem vinde!</h2>
-            <p className='text-xl text-black'>Aqui você encontra Medicamentos e Cosméticos</p>
+            <h2 className='text-5xl text-black font-bold'>olá, seja bem vinde!</h2>
+            <p className='text-xl text-black'>Aqui você encontra medicamentos e cosméticos</p>
 
             <div className="flex justify-around gap-4">
               <button className='rounded bg-indigo-900 text-white py-2 px-4'>Cadastrar produtos</button>
